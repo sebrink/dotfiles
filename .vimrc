72054887e88793add907d5693ef0f55bf65b73b8
Scott <se.brink15@gmail.com>
2019-06-14 18:52:51 -0400

    Fixed some formatting

--- .vimrc
+++ .vimrc
@@ -4,7 +4,7 @@
 
 """ Indentation
 set shiftwidth=4
-set softtabstop=4
+set tabstop=4
 set expandtab
 set smarttab
 filetype plugin indent on
--- setup.sh
+++ setup.sh
@@ -7,30 +7,56 @@
 #   - Set installed browser as default browser
 #   - Remove enter click on brew installation
 #   - Add slack, spotify, spectacle, vmware-fusion, signal, vlc, vivaldi, google chrome, firefox, virtualbox, postman, android studio
+#	- Add colors to the install messages
+#	- See if applescript will do anything better?
 
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install oh-my-zsh
-brew install zsh curl
-curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sh install.sh --unattended
-rm install.sh
 
 # Tap some useful things
 brew tap caskroom/cask
 brew tap caskroom/fonts
 
 # Other useful utilities
-brew install wget coreutils findutils tree git ssh-copy-id htop sl thefuck
+PACKAGES=(
+	wget
+	zsh
+	coreutils
+	findutils
+	tree
+	git
+	ssh-copy-id
+	htop
+	sl
+	thefuck
+	python
+	python3
+	vim
+)    
 
-# Install web browser (brave, vivaldi, chrome, firefox, etc.)
-brew cask install brave-browser 
+echo " [+] Installing Packages... "
+brew install ${PACKAGES[@]}
 
 # Useful applications 
-brew cask install iterm2 visual-studio-code vagrant docker
+CASKS=(
+	brave-browser
+	iterm2
+	visual-studio-code
+	vagrant
+	docker
+	font-hack-nerd-font
+)
+
+echo " [+] Installing Casks... "
+brew install ${CASKS[@]}
+
+echo " [+] Installing the oh-my-zsh... " 
+curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh install.sh --unattended
+rm install.sh
 
 # Themes
+echo " [+] Downloading themes and plugins... "
 git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k/
 ( cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower )
 
@@ -39,9 +65,7 @@ git clone https://github.com/amstrad/oh-my-matrix.git $HOME/.oh-my-zsh/custom/pl
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
 git clone https://github.com/floor114/zsh-apple-touchbar $HOME/.oh-my-zsh/custom/plugins/zsh-apple-touchbar/
 
-# Fonts (Not totally automatic, need to set in iterm2)
-brew cask install font-hack-nerd-font
-
+echo " [+] Setting up dotfiles... "
 # Setup dotfiles
 git clone https://github.com/sebrink/dotfiles.git $HOME/dotfiles 
 mv $HOME/dotfiles/.gitconfig $HOME/.gitconfig
@@ -50,7 +74,8 @@ ln -sv $HOME/dotfiles/.zshrc $HOME/.zshrc
 rm $HOME/.vimrc
 ln -sv $HOME/dotfiles/.vimrc $HOME/.vimrc
 
-# Sudo to fix colorls, make tetirs, set zsh
+# Colorls, make tetirs, set zsh
+echo " [+] Installing colorls, sedtris, and your new shell... "
 sudo gem install colorls
 sudo git clone https://github.com/uuner/sedtris.git /opt
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
