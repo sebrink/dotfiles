1023992f70d65cdac2232e477428b65008875197
Scott <se.brink15@gmail.com>
2019-07-13 00:00:32 -0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@
 #		- Virtualbox
 #		- Vagrant
 #		- MTMR
-#	- May just give a message to double check on the problem children (probably aren't installed right)
+#	- Fix Dock settings
+#		- show-recents = 0
+#		- launchanim = 0
 
 # Install brew
 echo " [+] Installing brew and tapping useful casks... "
@@ -39,6 +41,8 @@ brew install ${PACKAGES[@]}
 
 # Useful applications 
 CASKS=(
+	mtmr
+	slack
 	iterm2
 	visual-studio-code
 	docker
@@ -55,9 +59,7 @@ CASKS=(
 	font-hack-nerd-font
 	sloth
 	vagrant
-	mtmr
 	vagrant-manager
-	virtualbox
 )
 
 echo " [+] Installing Casks... "
@@ -86,7 +88,8 @@ rm $HOME/.zshrc
 ln -sv $HOME/dotfiles/.zshrc $HOME/.zshrc
 rm $HOME/.vimrc
 ln -sv $HOME/dotfiles/.vimrc $HOME/.vimrc
-rm $HOME/Library/Applications\ Support/MTMR/items.json
+# This works if the file exists or not!
+echo "" > $HOME/Library/Applications\ Support/MTMR/items.json
 ln -sv $HOME/dotfiles/items.json $HOME/Library/Application\ Support/MTMR/items.json
 
 # Colorls, make tetirs, set zsh
@@ -115,8 +118,6 @@ defaults write com.apple.finder QuitMenuItem -bool true
 defaults write com.apple.dock persistent-apps -array
 
 # Adding Icons to the Dock
-# THIS IS SO GOING TO BREAK LOL
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Brave Browser.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
@@ -124,13 +125,14 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual Studio Code.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spotify.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Messages.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Notes.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
 # Sets autohide on the dock
 defaults write com.apple.dock autohide -int 1
 
 # Sets dock to the left side of the screen
-defaults write com.appl.dock orientation left
+defaults write com.apple.dock orientation left
 
 # Disable the annoying line marks in iterm2
 defaults write com.apple.Terminal ShowLineMarks -int 0
@@ -138,4 +140,4 @@ defaults write com.apple.Terminal ShowLineMarks -int 0
 # Remove iterm2 prompt on quit
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-echo "\nInstall complete!\nCheck on the installs of MTMR, Vagrant, and Virtualbox. These may need to be done by hand.\nAlso, remember to change the font on iterm2 and set your default browser!"
+echo "Install complete!  Check on the installs of MTMR, Vagrant, and Virtualbox. These may need to be done by hand.Also, remember to change the font on iterm2 and set your default browser!"
