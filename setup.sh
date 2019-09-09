ac6009c589be2c9204169cc0b29e455526ecffa5
Scott <se.brink15@gmail.com>
2019-09-08 23:34:24 -0500

    Imagine forgetting to type -y

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ else
 
 	# Setting up for VSCode
 	apt update
-	apt install software-properties-common apt-transport-https wget
+	apt install software-properties-common apt-transport-https wget -y
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
